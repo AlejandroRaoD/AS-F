4a256f1b0cc39b0bc68349fff63b4038b49ce4d7
AlejandroRaoD <raulaocad@gmail.com>
2024-11-25 21:30:08 -0400

    otros

--- src/app/personal/page.tsx
+++ src/app/personal/page.tsx
@@ -1,21 +1,18 @@
-import NavBar from "../common/components/NavBar";
+import NavBar from "@/app/common/components/NavBar";
 
-export default function PerfilPage() {
+export default function PersonalPage() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
       {}
       <NavBar
-        navTitle="Página de Perfil"
-        hrefBackButton="/" 
-        showProfileButton={false}
+        navTitle="Gestión del personal"
+        hrefBackButton="/"
       />
 
       {/* Contenido principal */}
       <div className="flex flex-col items-center justify-center flex-grow">
-        <h1 className="text-4xl font-bold text-gray-800 mb-6">Página de Perfil</h1>
-        <p className="text-lg text-gray-600">
-          Aquí podrás gestionar los perfiles
-        </p>
+        <h1 className="text-4xl font-bold text-gray-800 mb-6">Página de Personal</h1>
+        <p className="text-lg text-gray-600">Info del Personal</p>
       </div>
     </div>
   );
