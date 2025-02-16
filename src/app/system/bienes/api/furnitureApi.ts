8245b7e58d96e32476cc826b95d02d05920f086d
AlejandroRaoD <raulaocad@gmail.com>
2025-02-16 15:34:56 -0400

    PREPRESENTACION

--- src/app/system/bienes/api/furnitureApi.ts
+++ src/app/system/bienes/api/furnitureApi.ts
@@ -8,27 +8,37 @@ import { UpdateFurnitureDto } from "../dto/update-furniture.dto";
 const url = `furniture`;
 
 export const getFurniture_Request = async (
-	id: string
-): PromiseFormatRes<furnitureAttributes> =>
-	(await abreuSystemAPI.get(`${url}/${id}`)).data;
+  id: string
+): Promise<PromiseFormatRes<furnitureAttributes>> => {
+  const response = await abreuSystemAPI.get(`${url}/${id}`);
+  return response.data;
+};
 
 export const getFurnitures_Request = async (
-	query?: QueryFurnitureDto
-): PromiseFormatRes<furnitureAttributes[]> =>
-	(await abreuSystemAPI.get(url, { params: query })).data;
+  query?: QueryFurnitureDto
+): Promise<PromiseFormatRes<furnitureAttributes[]>> => {
+  const response = await abreuSystemAPI.get(url, { params: query });
+  return response.data;
+};
 
 export const createFurniture_Request = async (
-	data: CreateFurnitureDto
-): PromiseFormatRes<furnitureAttributes> =>
-	(await abreuSystemAPI.post(url, data)).data;
+  data: CreateFurnitureDto
+): Promise<PromiseFormatRes<furnitureAttributes>> => {
+  const response = await abreuSystemAPI.post(url, data);
+  return response.data;
+};
 
 export const updateFurniture_Request = async (
-	id: string,
-	data: UpdateFurnitureDto
-): PromiseFormatRes<furnitureAttributes> =>
-	(await abreuSystemAPI.put(`${url}/${id}`, data)).data;
+  id: string,
+  data: UpdateFurnitureDto
+): Promise<PromiseFormatRes<furnitureAttributes>> => {
+  const response = await abreuSystemAPI.put(`${url}/${id}`, data);
+  return response.data;
+};
 
 export const deleteFurniture_Request = async (
-	id: string
-): PromiseFormatRes<furnitureAttributes> =>
-	(await abreuSystemAPI.delete(`${url}/${id}`)).data;
+  id: string
+): Promise<PromiseFormatRes<furnitureAttributes>> => {
+  const response = await abreuSystemAPI.delete(`${url}/${id}`);
+  return response.data;
+};
